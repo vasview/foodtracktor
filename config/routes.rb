bec745ab8799f5ed856deb257476a034d5a25252
Vasily Toropchin <vasview@gmail.com>
2016-09-12 21:55:51 +0600

    Level 2 completed.
    Pages controller with index and show actions were generated. Index and show views were configured.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  root 'places#index'
+
+  resources :places
+
   ActiveAdmin.routes(self)
   devise_for :users
-  root 'places#index'
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
